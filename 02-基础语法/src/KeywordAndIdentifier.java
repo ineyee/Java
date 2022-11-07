3071c1c1c939b5824ebbfaf9491022383ab87fa5
ineyee <ineyee@foxmail.com>
2022-11-07 20:53:41 +0800

    02-基础语法：05-关键字和标识符

---
+++ 02-基础语法/src/KeywordAndIdentifier.java
@@ -0,0 +1,24 @@
+/**
+ * 05-关键字和标识符
+ * @author yiyi
+ *
+ */
+public class KeywordAndIdentifier {
+
+	public static void main(String[] args) {
+		/*
+		 * Java里的关键字和标识符跟其它语言差不多。
+		 */
+		
+		/*
+		 * Java里的命名规范：
+		 * 1、项目名：大驼峰，MyNameAndAge
+		 * 2、文件夹名、文件名：大驼峰，MyNameAndAge
+		 * 3、类名：大驼峰，MyNameAndAge
+		 * 4、类的属性名、类的方法名：小驼峰，myNameAndAge
+		 * 5、变量名：小驼峰，myNameAndAge
+		 * 6、常量名：全部大写加下划线，MY_NAME_AND_AGE
+		 */
+	}
+
+}
