f3f0633428d26ac45acf870d797e795b9a8fcd49
ineyee <ineyee@foxmail.com>
2022-11-09 19:10:38 +0800

    03-面向对象：03-构造方法补充

--- 03-面向对象/src/ConstructorDog.java
+++ 03-面向对象/src/ConstructorDog.java
@@ -12,7 +12,7 @@ public class ConstructorDog {
 	 * 
 	 * 	public ConstructorDog() {}
 	 */
-	
+		
 	/*
 	 * 2、当我们自定义了构造方法时，那编译器就不会帮这个类自动生成无参的构造方法了，所以此时如果我们还想使用无参
 	 * 的构造方法就得自己写一个了
@@ -31,7 +31,7 @@ public class ConstructorDog {
 	}
 	
 	// 部分参数构造方法
-	public ConstructorDog(ConstructorDog this, int age) {
+	public ConstructorDog(int age) {
 		/* 
 		 * this知识点补充：所有的面向对象语言里，包括OC、Swift、Dart等，所有的方法都有一个默认的隐藏参数this，
 		 * 而且这个this参数还是在参数列表的第一个参数位置，就像OC和Swift里的self，Dart里的this一样，其实每个
@@ -41,7 +41,9 @@ public class ConstructorDog {
 		 * 
 		 * 为什么这样做？我们知道方法是存储在类内部的，而类编译成二进制后会加载到代码区，一个类的方法肯定只需要存一份
 		 * 就够了，那不同的对象调用同一个方法肯定应该打印出对象自己age，所以方法里怎么拿到这个对象呢？那就只能是参数
-		 * 传进来了，所以编译器就为所有的方法都搞了这么一个隐藏的参数this，代表方法调用者——即调用方法的那个对象。
+		 * 传进来了，所以编译器就为所有的方法都搞了这么一个隐藏的参数this，代表方法调用者——即调用方法的那个对象，这就类似于：
+		 * 
+		 * public ConstructorDog(ConstructorDog this, int age) { ... }
 		 */ 
 		this(age, 0);
 		System.out.println(age);
