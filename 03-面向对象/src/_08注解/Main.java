10d2fc68ec13f535a94f3b4cb432d87bbf493f7c
ineyee <ineyee@foxmail.com>
2022-11-13 17:02:26 +0800

    03-面向对象：08-注解

---
+++ 03-面向对象/src/_08注解/Main.java
@@ -0,0 +1,14 @@
+package _08注解;
+
+public class Main {
+
+	public static void main(String[] args) {
+		/*
+		 * Java里以@符号开头的东西就是注解，系统提供了很多注解，我们也可以自己定义注解。常见的注解：
+		 * 
+		 * 1、@Override：代表是子类重写了父类的方法
+		 * 2、@SuppressWarnings({"警告类别"})：消除警告用
+		 */
+	}
+
+}
