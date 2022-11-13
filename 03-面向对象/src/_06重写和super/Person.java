bd8427823985580aba9776c6bfbb9e341775da1a
ineyee <ineyee@foxmail.com>
2022-11-13 16:13:43 +0800

    03-面向对象：06-重写和super

---
+++ 03-面向对象/src/_06重写和super/Person.java
@@ -0,0 +1,11 @@
+package _06重写和super;
+
+public class Person {
+	public void speak() {
+		System.out.println("Person - speak");
+	}
+	
+	public void eat() {
+		System.out.println("Person - eat");
+	}
+}
---
+++ 03-面向对象/src/_06重写和super/Student.java
@@ -0,0 +1,22 @@
+package _06重写和super;
+
+public class Student extends Person {
+	/*
+	 * Java里子类重写父类里的方法也是用override
+	 */
+	@Override
+	public void speak() {
+		/*
+		 * Java里子类调用父类里的方法也是用super
+		 */
+		super.speak();
+		eat();
+		this.eat();
+		super.eat();
+	}
+	
+	@Override
+	public void eat() {
+		System.out.println("Student - eat");
+	}
+}
