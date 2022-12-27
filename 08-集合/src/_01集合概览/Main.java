b5fe56af4e8fa986439232d806e16fecd5d4c5b5
ineyee <ineyee@foxmail.com>
2022-12-27 14:59:54 +0800

    08-集合：01-List、Set、Map的区别

---
+++ 08-集合/src/_01集合概览/Main.java
@@ -0,0 +1,25 @@
+package _01集合概览;
+
+public class Main {
+	public static void main(String[] args) {
+
+	}
+	
+	public static void test1() {
+		/*
+		 * List、Set、Map的区别：
+		 * 
+		 * 1、List
+		 * （1）List是有序的，可以通过下标访问元素
+		 * （2）可以存储重复的元素
+		 * 
+		 * 2、Set
+		 * （1）Set是无序的，不可以通过下标访问元素
+		 * （2）不可以存储重复的元素
+		 * 
+		 * 3、Map
+		 * （1）Map也不会记录key-value对的顺序，所以也是无序的，不可以通过下标访问key-value
+		 * （2）Map里不可以存储重复的key
+		 */
+	}
+}
